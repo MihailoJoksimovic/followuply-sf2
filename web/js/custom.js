e4fdc0c762dc3f1c2eb50d0f55c2673731df0b4d
Followuply Server Bastard <followuply-server@followuply.com>
2016-02-10 10:43:47 -0500

    Add web site design
    Squashed commit of the following:
    
    commit fabb8e50ca01440126b034bea390d5dfac9a463b
    Author: Followuply Server Bastard <followuply-server@followuply.com>
    Date:   Wed Feb 10 10:42:57 2016 -0500
    
        Changes from Stefan & Stefan
    
    commit da4f6266731e6a6653de6a36367cb091c4aa58e9
    Merge: 99dbf65 354d363
    Author: Followuply Server Bastard <followuply-server@followuply.com>
    Date:   Tue Feb 9 13:44:19 2016 -0500
    
        Merge branch 'master' of github.com:MihailoJoksimovic/followuply-sf2 into design-playground
    
        Conflicts:
        	app/Resources/views/base.html.twig
        	app/config/config.yml
    
    commit 99dbf65e57a80ad600afd1a2cf2c4aae5aa42e2f
    Author: Followuply Server Bastard <followuply-server@followuply.com>
    Date:   Tue Feb 9 13:43:19 2016 -0500
    
        Changes from Stefan & Stefan

--- web/js/custom.js
+++ web/js/custom.js
@@ -86,5 +86,89 @@ jQuery(function () {
         }
         return false;
     });	
+
+setTimeout(function(){
+  $('.typewrite2').css('display', 'inline-block');
+}, 5500);
+
+setTimeout(function(){
+  $('.typewrite3').css('display', 'inline-block');
+}, 8000);
 	
 }());
+
+//Video Script Bellow
+
+var vid = document.getElementById("bgvid");
+var pauseButton = document.querySelector("#polina button");
+
+function vidFade() {
+  vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+{
+// only functional if "loop" is removed 
+vid.pause();
+// to capture IE10
+vidFade();
+}); 
+
+// Typewriter Script Bellow
+
+var TxtType = function(el, toRotate, period) {
+		        this.toRotate = toRotate;
+		        this.el = el;
+		        this.loopNum = 0;
+		        this.period = parseInt(period, 10) || 2000;
+		        this.txt = '';
+		        this.tick();
+		        this.isDeleting = false;
+		    };
+		
+		    TxtType.prototype.tick = function() {
+		        var i = this.loopNum % this.toRotate.length;
+		        var fullTxt = this.toRotate[i];
+		
+		        if (this.isDeleting) {
+		        this.txt = fullTxt.substring(0, this.txt.length - 1);
+		        } else {
+		        this.txt = fullTxt.substring(0, this.txt.length + 1);
+		        }
+		
+		        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+		
+		        var that = this;
+		        var delta = 200 - Math.random() * 100;
+		
+		        if (this.isDeleting) { delta /= 2; }
+		
+		        if (!this.isDeleting && this.txt === fullTxt) {
+		        delta = this.period;
+		        this.isDeleting = true;
+		        } else if (this.isDeleting && this.txt === '') {
+		        this.isDeleting = false;
+		        this.loopNum++;
+		        delta = 500;
+		        }
+		
+		        setTimeout(function() {
+		        that.tick();
+		        }, delta);
+		    };
+		
+		    window.onload = function() {
+		        var elements = document.getElementsByClassName('typewrite');
+		        for (var i=0; i<elements.length; i++) {
+		            var toRotate = elements[i].getAttribute('data-type');
+		            var period = elements[i].getAttribute('data-period');
+		            if (toRotate) {
+		              new TxtType(elements[i], JSON.parse(toRotate), period);
+		            }
+		        }
+		        // INJECT CSS
+		        var css = document.createElement("style");
+		        css.type = "text/css";
+		        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+		        document.body.appendChild(css);
+		    };
